4fd1e175c90e60b853dc23b6a22514854bb6da52
Abrahim Gaber <abrahimagaber@gmail.com>
2023-05-01 17:24:27 -0400

    Moved files out of directories.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-// import * as bits from '@/bits/bits';
-import * as arrays from './arrays/arrays';
-import * as bits from './bits/bits';
+// import * as bits from '@/bits';
+import * as arrays from './arrays';
+import * as bits from './bits';
 
 console.log(bits.fromBinaryString('11111111111111111111111111111101'));
