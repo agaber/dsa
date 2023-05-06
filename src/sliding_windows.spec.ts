fcd39984341206936d81660478498fb04166d478
Abrahim Gaber <abrahimagaber@gmail.com>
2023-05-06 14:53:48 -0400

    Minimum Window Substring in sliding_windows.ts.

--- src/sliding_windows.spec.ts
+++ src/sliding_windows.spec.ts
@@ -15,11 +15,34 @@ describe('Longest Substring Without Repeating Characters', () => {
   });
 });
 
-// 
+
 describe('Longest Repeating Character Replacement', () => {
   it('should return the longest repeating char replacements', () => {
     expect(sw.characterReplacement('ABAB', 2)).toEqual(4);
     expect(sw.characterReplacement('AABABBA', 1)).toEqual(4);
     expect(sw.characterReplacement('ABACD', 2)).toEqual(4);
+  });
+});
+
+describe('Minimum Window Substring', () => {
+  it('should pass test case 1', () => {
+    expect(sw.minWindow('ADOBECODEBANC', 'ABC')).toEqual('BANC');
+  });
+
+  it('should pass test case 2', () => {
+    expect(sw.minWindow('a', 'a')).toEqual('a');
+  });
+
+  it('should pass test case 3', () => {
+    expect(sw.minWindow('a', 'aa')).toEqual('');
+  });
+
+  it('should pass test case 4', () => {
+    expect(sw.minWindow('ADOBECODEBANCDDD', 'ABC')).toEqual('BANC');
+  });
+
+  it('should pass test case 5', () => {
+    expect(sw.minWindow('KADOBECODEBANCDDDEI', 'ABCEKI'))
+      .toEqual('KADOBECODEBANCDDDEI');
   });
 });
--- src/sliding_windows.ts
+++ src/sliding_windows.ts
@@ -92,3 +92,53 @@ export function characterReplacement(s: string, k: number): number {
     return Math.max(...charCounts.values());
   }
 };
+
+/**
+ * Minimum Window Substring
+ * 
+ * Given two strings s and t of lengths m and n respectively, return the minimum
+ * window substring of s such that every character in t (including duplicates) is
+ * included in the window. If there is no such substring, return the empty
+ * string "".
+ * 
+ * The testcases will be generated such that the answer is unique.
+ * 
+ * https://leetcode.com/problems/minimum-window-substring/
+ * https://www.pramp.com/challenge/wqNo9joKG6IJm67B6z34
+ */
+export function minWindow(s: string, t: string): string {
+  const tcount = new Map<string, number>();
+  [...t].forEach(str => tcount.set(str, (tcount.get(str) || 0) + 1));
+  const need = t.length;
+
+  const scount = new Map<string, number>();
+  let have = 0;
+  let minSubstring = '';
+  let l = 0;
+
+  for (let r = 0; r < s.length; r++) {
+    if (tcount.has(s[r])) {
+      scount.set(s[r], (scount.get(s[r]) || 0) + 1);
+      if (scount.get(s[r])! <= tcount.get(s[r])!) {
+        have++;
+      }
+    }
+
+    while (have === need) {
+      const substringLength = r - l + 1;
+      if (minSubstring.length === 0 || substringLength < minSubstring.length) {
+        minSubstring = s.substring(l, l + substringLength);
+      }
+
+      if (scount.has(s[l])) {
+        scount.set(s[l], scount.get(s[l])! - 1);
+        if (scount.get(s[l])! < tcount.get(s[l])!) {
+          have--;
+        }
+      }
+      l++;
+    }
+  }
+
+  return minSubstring;
+};
