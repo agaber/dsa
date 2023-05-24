b69cfdbbe87206c31e17f945df3718c07c6e6845
Abrahim Gaber <abrahimagaber@gmail.com>
2023-05-24 08:03:03 -0400

    Added a few more arrays/hashing exercises.

--- kotlin/src/main/kotlin/dev/agaber/dsa/arrays.kt
+++ kotlin/src/main/kotlin/dev/agaber/dsa/arrays.kt
@@ -1,5 +1,7 @@
 package dev.agaber.dsa
 
+import java.util.PriorityQueue
+
 /**
  * Contains Duplicate
  *
@@ -11,9 +13,11 @@ package dev.agaber.dsa
  * - -109 <= nums[i] <= 109
  *
  * Attributes
- * - List: Bline 75
+ * - List: Blind 75
  * - Level: Easy
  * - Link: https://leetcode.com/problems/contains-duplicate/
+ * - Time: O(n)
+ * - Space: O(n)
  */
 fun containsDuplicate(nums: IntArray): Boolean {
   val set = mutableSetOf<Int>()
@@ -25,3 +29,164 @@ fun containsDuplicate(nums: IntArray): Boolean {
   }
   return false
 }
+
+/**
+ * Valid Anagram
+ *
+ * Given two strings s and t, return true if t is an anagram of s, and false
+ * otherwise.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a
+ * different word or phrase, typically using all the original letters exactly
+ * once.
+ *
+ * Constraints:
+ * - 1 <= s.length, t.length <= 5 * 104
+ * - s and t consist of lowercase English letters.
+ *
+ * Attributes
+ *  - List: Blind 75
+ *  - Level: Easy
+ *  - Link: https://leetcode.com/problems/valid-anagram/
+ *  - Time: O(n)
+ *  - Space: O(n)
+ */
+fun isAnagram(s: String, t: String): Boolean {
+  if (s.length != t.length) {
+    return false;
+  }
+  val counter: MutableMap<Char, Int> = mutableMapOf()
+  for (i in s.indices) {
+    counter[s[i]] = counter.getOrDefault(s[i], 0) + 1
+    counter[t[i]] = counter.getOrDefault(t[i], 0) - 1
+    if (counter[s[i]] == 0) {
+      counter.remove(s[i])
+    }
+    if (counter[t[i]] == 0) {
+      counter.remove(t[i])
+    }
+  }
+  return counter.isEmpty()
+}
+
+/**
+ * Two Sum
+ *
+ * Given an array of integers nums and an integer target, return indices of the
+ * two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ *
+ * You can return the answer in any order.
+ *
+ * Constraints
+ * - 2 <= nums.length <= 104
+ * - -109 <= nums[i] <= 109
+ * - -109 <= target <= 109
+ * - Only one valid answer exists.
+ *
+ * Attributes
+ *  - List: Blind 75
+ *  - Level: Easy
+ *  - Link: https://leetcode.com/problems/two-sum/
+ *  - Time: O(n)
+ *  - Space: O(n)
+ */
+fun twoSum(nums: IntArray, target: Int): IntArray {
+  val map: MutableMap<Int, Int> = mutableMapOf()
+  for (i in nums.indices) {
+    val compliment = target - nums[i]
+    if (map.containsKey(compliment)) {
+      return intArrayOf(map[compliment]!!, i)
+    }
+    map[nums[i]] = i
+  }
+  return intArrayOf()
+}
+
+/**
+ * Group Anagrams
+ *
+ * Given an array of strings strs, group the anagrams together. You can return
+ * the answer in any order.
+ *
+ * An Anagram is a word or phrase formed by rearranging the letters of a
+ * different word or phrase, typically using all the original letters exactly
+ * once.
+ *
+ * Constraints
+ * - 1 <= strs.length <= 104
+ * - 0 <= strs[i].length <= 100
+ * - strs[i] consists of lowercase English letters.
+ *
+ * Attributes
+ *  - List: Blind 75
+ *  - Level: Medium
+ *  - Link: https://leetcode.com/problems/group-anagrams/
+ *  - Time Complexity: O(NK), where N is the length of strs, and K is the
+ *    maximum length of a string in strs. Counting each string is linear in the
+ *    size of the string, and we count every string.
+ *  - Space Complexity: O(NK), the total information content stored in ans.
+ */
+fun groupAnagrams(strs: Array<String>): List<List<String>> {
+  fun hash(str: String): String {
+    val arr = IntArray(26)
+    for (c in str) {
+      arr[c - 'a'] += 1;
+    }
+    return arr.map{ c -> "#$c#" }.joinToString("")
+  }
+
+  val anagrams: MutableMap<String, MutableList<String>> = mutableMapOf();
+  for (str in strs) {
+    val key = hash(str)
+    anagrams.computeIfAbsent(key) { mutableListOf() }.add(str)
+  }
+  val result: MutableList<List<String>> = mutableListOf()
+  anagrams.values.forEach{ lst -> result.add(lst) }
+  return result.toList()
+}
+
+/**
+ * Top K Frequent Elements
+ *
+ * Given an integer array nums and an integer k, return the k most frequent
+ * elements. You may return the answer in any order.
+ *
+ * Constraints
+ * - 1 <= nums.length <= 105
+ * - -104 <= nums[i] <= 104
+ * - k is in the range [1, the number of unique elements in the array].
+ * - It is guaranteed that the answer is unique.
+ *
+ * Attributes
+ *  - List: Blind 75
+ *  - Level: Medium
+ *  - Link: https://leetcode.com/problems/top-k-frequent-elements/
+ *  - Time: O(n log k)
+ *  - Space: O(n + k)
+ */
+fun topKFrequent(nums: IntArray, k: Int): IntArray {
+  val counter: MutableMap<Int, Int> = mutableMapOf()
+  for (i in nums.indices) {
+    counter[nums[i]] = counter.getOrDefault(nums[i], 0) + 1
+  }
+
+  val comparator: Comparator<IntArray> = compareBy { it[1] }
+  val pq: PriorityQueue<IntArray> = PriorityQueue<IntArray>(comparator)
+  for (e in counter.entries) {
+    pq.add(intArrayOf(e.key, e.value))
+    if (pq.size > k) {
+      pq.poll()
+    }
+  }
+
+  val size = if (k > pq.size) pq.size else k
+  val answer = IntArray(size)
+  for (i in (size - 1).downTo(0)) {
+    answer[i] = pq.poll()[0]
+  }
+  return answer
+}
+
--- kotlin/src/test/kotlin/dev/agaber/dsa/ArraysKtTest.kt
+++ kotlin/src/test/kotlin/dev/agaber/dsa/ArraysKtTest.kt
@@ -1,7 +1,6 @@
 package dev.agaber.dsa
 
-import org.junit.jupiter.api.Assertions.assertFalse
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -16,5 +15,36 @@ class ArraysKtTest {
     assertTrue(containsDuplicate(intArrayOf(1, 2, 3, 1)))
     assertFalse(containsDuplicate(intArrayOf(1, 2, 3, 4)))
     assertTrue(containsDuplicate(intArrayOf(1, 1, 1, 3, 3, 4, 3, 2, 4, 2)))
+  }
+
+  @Test
+  fun testIsAnagram() {
+    assertTrue(isAnagram("anagram", "nagaram"))
+    assertFalse(isAnagram("rat", "car"))
+  }
+
+  @Test
+  fun testTwoSum() {
+    assertArrayEquals(twoSum(intArrayOf(2, 7, 11, 15), 9), intArrayOf(0, 1))
+    assertArrayEquals(twoSum(intArrayOf(3, 2, 4), 6), intArrayOf(1, 2))
+    assertArrayEquals(twoSum(intArrayOf(3, 3), 6), intArrayOf(0, 1))
+  }
+
+  @Test
+  fun testGroupAnagrams() {
+    assertEquals(
+      groupAnagrams(arrayOf("eat", "tea", "tan", "ate", "nat", "bat")),
+      arrayListOf(
+        arrayListOf("eat", "tea", "ate"),
+        arrayListOf("tan", "nat"),
+        arrayListOf("bat")))
+    assertEquals(groupAnagrams(arrayOf("")), arrayListOf(arrayListOf("")))
+    assertEquals(groupAnagrams(arrayOf("a")), arrayListOf(arrayListOf("a")))
+  }
+
+  @Test
+  fun testTopKFrequent() {
+    assertArrayEquals(topKFrequent(intArrayOf(1, 1, 1, 2, 2, 3), 2), intArrayOf(1, 2))
+    assertArrayEquals(topKFrequent(intArrayOf(1), 1), intArrayOf(1))
   }
 }
